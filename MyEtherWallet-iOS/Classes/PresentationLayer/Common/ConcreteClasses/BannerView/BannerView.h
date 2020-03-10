9050840f584a3bd7852537adcab6b98f8a0a1bf6
Mikhail Nikanorov <fobozsb@gmail.com>
2020-03-09 19:10:35 -0700

    Implemented MEWwallet banner

---
+++ MyEtherWallet-iOS/Classes/PresentationLayer/Common/ConcreteClasses/BannerView/BannerView.h
@@ -0,0 +1,18 @@
+//
+//  BannerView.h
+//  MyEtherWallet-iOS
+//
+//  Created by Mikhail Nikanorov on 3/6/20.
+//  Copyright © 2020 MyEtherWallet, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BannerView : UIView
+- (void) playAnimation;
+- (void) stopAnimation;
+@end
+
+NS_ASSUME_NONNULL_END
